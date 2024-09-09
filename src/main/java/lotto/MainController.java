b8867afe5c5a794320cb1abe39141b0a4e254546
hyojun <hjlim45678@gmail.com>
2024-09-09 09:30:31 +0900

    feat: 패키지 구조 설계 완료

---
+++ src/main/java/lotto/MainController.java
@@ -0,0 +1,6 @@
+package lotto;
+
+public class MainController {
+    public static void run() {
+    }
+}
---
+++ src/main/java/lotto/domain/User.java
@@ -0,0 +1,8 @@
+package lotto.domain;
+
+public class User {
+
+    BounusNumber bounusNumber = new BounusNumber();
+    LottoBudget lottoBudget = new LottoBudget();
+    WinningLotto winningLotto = new WinningLotto();
+}
