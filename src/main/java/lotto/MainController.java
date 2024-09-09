b9dffcc8c6fec9591d0e9971d0c982785b7f8676
hyojun <hjlim45678@gmail.com>
2024-09-09 12:22:40 +0900

    feat: 구매금액 입력 기능 중 1000원 단위 검증 구현

--- src/main/java/lotto/MainController.java
+++ src/main/java/lotto/MainController.java
@@ -3,11 +3,29 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import java.util.stream.IntStream;
 import lotto.domain.Budget;
+import lotto.domain.Lotto;
 import lotto.view.InputView;
 
 public class MainController {
+
     public static void run() {
-        Budget budget = new Budget(InputView.readInteger("구입금액을 입력해 주세요."));
+        Budget budget = inputBudget();
+
     }
+
+    private static Budget inputBudget() {
+        Budget budget = null;
+        while (budget == null) {
+            try {
+                budget = new Budget(InputView.readInteger("구입금액을 입력해 주세요."));
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] " + e.getMessage() + " 다시 입력해 주세요.");
+            }
+        }
+        return budget;
+    }
+
+
 }
--- src/main/java/lotto/domain/Budget.java
+++ src/main/java/lotto/domain/Budget.java
@@ -1,9 +1,20 @@
 package lotto.domain;
 
+import lotto.view.InputView;
+
 public class Budget {
     private int amount;
 
     public Budget(int amount) {
+        validateBudget(amount);
         this.amount = amount;
+    }
+    private static void validateBudget(int amount) {
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("1000원 단위로만 금액을 입력해주세요.");
+        }
+    }
+    public int getAmount() {
+        return amount;
     }
 }
